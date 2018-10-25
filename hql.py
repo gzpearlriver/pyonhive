06ecab42726e73d346c3b62fcb319852f4e9930f
Francis Lu <frank@139.com>
2018-10-25 11:27:58 +0800

    dns inquiry

--- hql.py
+++ hql.py
@@ -8,8 +8,8 @@ def conturct_sel_content(groupby_col, sum_col):
     return '%s %s' % (groupby_col, sum_col) 
 
 def constuct_con(slicetime, cityname, other_cond=''):
-    time_con =  "slicetime == '%s' " % (slicetime)
-    city_con = "cityname == '%s'" % cityname
+    time_con =  "slicetime = '%s' " % (slicetime)
+    city_con = "cityname = '%s'" % cityname
     if other_cond == '':
         cond = "%s and %s" % (city_con , time_con)
     else:
@@ -56,7 +56,7 @@ def exec_hive(hql_file, result_file, op='a'):
     cmd_del = 'rm %s' % tmp_merge
     os.system(cmd_del)
 
-    cmd_hive = 'beeline -u "jdbc:hive2://master03.cluster-b.gdyd.com:10000/default;principal=hive/_HOST@GDSAI.COM?tez.queue.name=xingneng_exam2017_001"  --outputformat=csv2 --silent=true -f %s >%s' % (hql_file, tmp_result)
+    cmd_hive = 'beeline -u "jdbc:hive2://master03.cluster-b.gdyd.com:10000/default;principal=hive/_HOST@GDSAI.COM?tez.queue.name=xingneng_exam2017_001"  --outputformat=dsv --silent=true -f %s >%s' % (hql_file, tmp_result)
     os.system(cmd_hive)
 
     if op == 'a':
@@ -74,7 +74,9 @@ def exec_hive(hql_file, result_file, op='a'):
 
 
 def analyze_apptype_host(cities,times,apptype,appsubtype,hql_filename,result_filename):
-
+    cmd_del = 'rm %s' % result_filename
+    os.system(cmd_del)
+    
     #paramenter for this inquirey
     sourcetable = "mlte_s1u_http"
     
@@ -104,7 +106,9 @@ def analyze_apptype_host(cities,times,apptype,appsubtype,hql_filename,result_fil
             
             
 def analyze_apptype_uri(cities,times,apptype,appsubtype,hql_filename,result_filename):
-
+    cmd_del = 'rm %s' % result_filename
+    os.system(cmd_del)
+    
     #paramenter for this inquirey
     sourcetable = "mlte_s1u_http"
     
@@ -133,8 +137,10 @@ def analyze_apptype_uri(cities,times,apptype,appsubtype,hql_filename,result_file
             exec_hive(hql_filename, result_filename, op='a')
 
 
-def analyze_speed_apptype_cell(cities,times,apptype,appsubtype,hql_filename,result_filename):
-
+def analyze_speed_apptype_cell(cities,times,apptype,appsubtype,hql_filename='tmp_hql',result_filename='tmp_result',percentile='n'):
+    cmd_del = 'rm %s' % result_filename
+    os.system(cmd_del)
+    
     #paramenter for this inquirey
     sourcetable = "mlte_s1u_http"
     
@@ -142,8 +148,9 @@ def analyze_speed_apptype_cell(cities,times,apptype,appsubtype,hql_filename,resu
     sum(dl_data) dl_vol, \
     sum(ul_data) ul_vol,\
     SUM(http_lastpacket_1streq_delay)/1000  time, \
-    SUM(dl_data)/SUM(http_lastpacket_1streq_delay)*8*1000/1024 speed, \
-    percentile_approx(dl_data/http_lastpacket_1streq_delay*8*1000/1024, array(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)) percentile '
+    SUM(dl_data)/SUM(http_lastpacket_1streq_delay)*8*1000/1024 speed'
+    if (percentile=='y' or percentil=='Y'):
+        sum_col = sum_col + ', percentile_approx(dl_data/http_lastpacket_1streq_delay*8*1000/1024, array(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)) percentile '
 
     groupby_col = 'cityname, slicetime, app_type, APP_SUB_TYPE, TRANSACTION_TYPE, cell_id'
     groupby = construct_groupby(groupby_col)
@@ -161,8 +168,10 @@ def analyze_speed_apptype_cell(cities,times,apptype,appsubtype,hql_filename,resu
             write_hql(hql, hql_filename)
             exec_hive(hql_filename, result_filename, op='a')
 
-def analyze_speed_apptype(cities,times,apptype=0,appsubtype=0,hql_filename='tmp_hql',result_filename='tmp_result'):
-
+def analyze_speed_apptype(cities,times,apptype=0,appsubtype=0,hql_filename='tmp_hql',result_filename='tmp_result',percentile='n'):
+    cmd_del = 'rm %s' % result_filename
+    os.system(cmd_del)
+    
     #paramenter for this inquirey
     sourcetable = "mlte_s1u_http"
     
@@ -170,8 +179,9 @@ def analyze_speed_apptype(cities,times,apptype=0,appsubtype=0,hql_filename='tmp_
     sum(dl_data) dl_vol, \
     sum(ul_data) ul_vol,\
     SUM(http_lastpacket_1streq_delay)/1000  time, \
-    SUM(dl_data)/SUM(http_lastpacket_1streq_delay)*8*1000/1024 speed, \
-    percentile_approx(dl_data/http_lastpacket_1streq_delay*8*1000/1024, array(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)) percentile '
+    SUM(dl_data)/SUM(http_lastpacket_1streq_delay)*8*1000/1024 speed'
+    if (percentile=='y' or percentil=='Y'):
+        sum_col = sum_col + ', percentile_approx(dl_data/http_lastpacket_1streq_delay*8*1000/1024, array(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)) percentile '
 
    
     groupby_col = 'cityname, slicetime, app_type, APP_SUB_TYPE, TRANSACTION_TYPE '
@@ -194,8 +204,42 @@ def analyze_speed_apptype(cities,times,apptype=0,appsubtype=0,hql_filename='tmp_
             hql = hql_selecttable(sourcetable, sel_content, cond, groupby, orderby)
             write_hql(hql, hql_filename)
             exec_hive(hql_filename, result_filename, op='a')
+
+def collect_domainname(cities,times,hql_filename='tmp_dns_hql',result_filename='domainname.csv'):
+    '''collect domain name form dns table'''
+    cmd_del = 'rm %s' % result_filename
+    os.system(cmd_del)
+    
+    #paramenter for this inquirey
+    sourcetable = 'mlte_s1u_dns'
+    sel_content = 'cityname, slicetime,apn,request_query_domain,query_result_ip'
+ 
+    for cityname in cities:
+        for slicetime in times:
+            cond = constuct_con(slicetime, cityname)
+            hql = hql_selecttable(sourcetable, sel_content, cond)
+            write_hql(hql, hql_filename)
+            exec_hive(hql_filename, result_filename, op='a')
+            
+def inquire_ip2domain(cities,times,ip,hql_filename='tmp_dns_hql',result_filename='domainname.csv'):
+    '''collect domain name form dns table'''
+    cmd_del = 'rm %s' % result_filename
+    os.system(cmd_del)
+    
+    #paramenter for this inquirey
+    sourcetable = 'mlte_s1u_dns'
+    sel_content = 'cityname, slicetime,apn,request_query_domain,query_result_ip'
+    other_cond = 'query_result_ip like "%s"' % ip 
+ 
+    for cityname in cities:
+        for slicetime in times:
+            cond = constuct_con(slicetime, cityname, other_cond)
+            hql = hql_selecttable(sourcetable, sel_content, cond)
+            write_hql(hql, hql_filename)
+            exec_hive(hql_filename, result_filename, op='a')
             
             
+allcities =['GZ','SZ','DG','FS','QY','SG','ST','CZ','JY','HZ','SW','HY','YF','MZ','JM','ZS','ZH','ZQ','YJ','MM','ZJ']
 cities  = ['QY','MM']
 times = ['2018102022']
 apptype =5
@@ -218,13 +262,21 @@ apptype =5
 appsubtype =17
 hql_filename = 'select_table.hql'
 result_filename = 'cell_speed.csv'
-analyze_speed_apptype_cell(cities,times,apptype,appsubtype,hql_filename,result_filename)
+#analyze_speed_apptype_cell(cities,times,apptype,appsubtype,hql_filename,result_filename)
 
 
-cities  = ['MM']
+cities  = ['MM','SG']
 times = ['2018102022']
 apptype =0   #all
 appsubtype =0  #all
 hql_filename = 'select_table.hql'
 result_filename = 'apptype_speeed.csv'
-#analyze_speed_apptype(cities,times,apptype,appsubtype,hql_filename,result_filename)
+#analyze_speed_apptype(cities,times,apptype,appsubtype,hql_filename,result_filename,percentile='y')
+
+cities  = ['MM']
+times = ['2018102322']
+hql_filename = 'select_table.hql'
+result_filename = 'ip2dns.csv'
+#collect_domainname(cities,times,hql_filename,result_filename)
+ip = '106.75.26.39'
+inquire_ip2domain(cities,times,ip)
