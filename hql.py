4c510fdf1d74b12f707dbbea867cfbef685870a5
Francis Lu <frank@139.com>
2018-10-30 09:08:24 +0800

    add main.py modify hql.py for ip inquiry

--- hql.py
+++ hql.py
@@ -1,6 +1,26 @@
 ''' this python file is going to excute the hive command'''
 import os
 
+def ip_dec2dot(ip_dec):
+    ip_dec = long(ip_dec)
+    ip_num1 = ip_dec // 256**3
+    ip_num2 = (ip_dec % 256**3) // 256**2
+    ip_num3 = (ip_dec % 256**2) // 256
+    ip_num4 = ip_dec % 256
+    ip_dot = "%s.%s.%s.%s" % (ip_num1,ip_num2,ip_num3,ip_num4)
+    print(ip_dot)
+    return(ip_dot)
+
+    
+def ip_dot2dec(ip_dot):
+    ip_num = ip_dot.split(".")
+    ip_dec = 0
+    for i in range(4):
+        ip_dec =ip_dec * 256 + int(ip_num[i])
+    print(ip_dec)
+    return(str(ip_dec))
+    
+
 def construct_groupby(groupby_col):
     return('group by %s' % groupby_col)
 
@@ -205,6 +225,47 @@ def analyze_speed_apptype(cities,times,apptype=0,appsubtype=0,hql_filename='tmp_
             write_hql(hql, hql_filename)
             exec_hive(hql_filename, result_filename, op='a')
 
+
+    
+def analyze_speed_ip_host(cities,times,ip_dec,apptype=0,appsubtype=0,hql_filename='tmp_hql',result_filename='tmp_result',percentile='n'):
+    #specify the ip, collect speed of all the apptype and host 
+    cmd_del = 'rm %s' % result_filename
+    os.system(cmd_del)
+    
+    #paramenter for this inquirey
+    sourcetable = "mlte_s1u_http"
+    
+    sum_col = ', COUNT(*) attemp, \
+    sum(dl_data) dl_vol, \
+    sum(ul_data) ul_vol,\
+    SUM(http_lastpacket_1streq_delay)/1000  time, \
+    SUM(dl_data)/SUM(http_lastpacket_1streq_delay)*8*1000/1024 speed'
+    if (percentile=='y' or percentile=='Y'):
+        sum_col = sum_col + ', percentile_approx(dl_data/http_lastpacket_1streq_delay*8*1000/1024, array(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)) percentile '
+
+   
+    groupby_col = 'cityname, slicetime, app_server_ip_ipv4, app_type, APP_SUB_TYPE, host, TRANSACTION_TYPE '
+    groupby = construct_groupby(groupby_col)
+
+    sel_content = conturct_sel_content(groupby_col, sum_col)
+
+    if apptype == 0:
+        other_cond = " dl_data >500000 and app_server_ip_ipv4 = '%s'" %  ip_dec
+    elif appsubtype == 0:
+        other_cond = " app_type=%s and dl_data >500000 and app_server_ip_ipv4 = '%s'" % (apptype ,ip_dec)
+    else:
+        other_cond = " app_type=%s and  APP_SUB_TYPE=%s and dl_data >500000 and app_server_ip_ipv4 = '%s'" % (apptype,appsubtype,ip_dec)
+
+    orderby = 'order by attemp desc'
+    
+    for cityname in cities:
+        for slicetime in times:
+            cond = constuct_con(slicetime, cityname, other_cond)
+            hql = hql_selecttable(sourcetable, sel_content, cond, groupby, orderby)
+            write_hql(hql, hql_filename)
+            exec_hive(hql_filename, result_filename, op='a')
+            
+            
 def collect_domainname(cities,times,hql_filename='tmp_dns_hql',result_filename='domainname.csv'):
     '''collect domain name form dns table'''
     cmd_del = 'rm %s' % result_filename
@@ -255,57 +316,3 @@ def inquire_domain2ip(cities,times,domain,hql_filename='tmp_dns_hql',result_file
             write_hql(hql, hql_filename)
             exec_hive(hql_filename, result_filename, op='a')
             
-allcities =['GZ','SZ','DG','FS','QY','SG','ST','CZ','JY','HZ','SW','HY','YF','MZ','JM','ZS','ZH','ZQ','YJ','MM','ZJ']
-cities  = ['QY','MM']
-times = ['2018102022']
-apptype =5
-appsubtype =17
-hql_filename = 'select_table.hql'
-result_filename = 'select_result.csv'
-#analyze_apptype_host(cities,times,apptype,appsubtype,hql_filename,result_filename)
-
-cities  = ['QY','MM']
-times = ['2018102022']
-apptype =5
-appsubtype =17
-hql_filename = 'select_table.hql'
-result_filename = 'select_result.csv'
-#analyze_apptype_uri(cities,times,apptype,appsubtype,hql_filename,result_filename)
-
-cities  = ['MM']
-times = ['2018102022']
-apptype =5
-appsubtype =17
-hql_filename = 'select_table.hql'
-result_filename = 'cell_speed.csv'
-#analyze_speed_apptype_cell(cities,times,apptype,appsubtype,hql_filename,result_filename)
-
-
-cities  = ['MM','SG']
-times = ['2018102022']
-apptype =0   #all
-appsubtype =0  #all
-hql_filename = 'select_table.hql'
-result_filename = 'apptype_speeed.csv'
-#analyze_speed_apptype(cities,times,apptype,appsubtype,hql_filename,result_filename,percentile='y')
-
-cities  = ['MM']
-times = ['2018102322']
-hql_filename = 'select_table.hql'
-result_filename = 'dnsxdr.csv'
-#collect_domainname(cities,times,hql_filename,result_filename)
-
-cities  = ['MM']
-times = ['2018102322']
-hql_filename = 'select_table.hql'
-result_filename = 'dnsxdr.csv'
-#collect_domainname(cities,times,hql_filename,result_filename)
-ip = '106.75.26.39'
-inquire_ip2domain(cities,times,ip)
-
-cities  = ['MM']
-times = ['2018102322']
-hql_filename = 'select_table.hql'
-result_filename = 'dnsxdr.csv'
-domain = 'www.baidu.com'
-inquire_domain2ip(cities,times,domain)
---
+++ main.py
@@ -0,0 +1,64 @@
+from hql import * 
+
+allcities =['GZ','SZ','DG','FS','QY','SG','ST','CZ','JY','HZ','SW','HY','YF','MZ','JM','ZS','ZH','ZQ','YJ','MM','ZJ']
+cities  = ['QY','MM']
+times = ['2018102022']
+apptype =5
+appsubtype =17
+hql_filename = 'select_table.hql'
+result_filename = 'select_result.csv'
+#analyze_apptype_host(cities,times,apptype,appsubtype,hql_filename,result_filename)
+
+cities  = ['QY','MM']
+times = ['2018102022']
+apptype =5
+appsubtype =17
+hql_filename = 'select_table.hql'
+result_filename = 'select_result.csv'
+#analyze_apptype_uri(cities,times,apptype,appsubtype,hql_filename,result_filename)
+
+cities  = ['MM']
+times = ['2018102022']
+apptype =5
+appsubtype =17
+hql_filename = 'select_table.hql'
+result_filename = 'cell_speed.csv'
+#analyze_speed_apptype_cell(cities,times,apptype,appsubtype,hql_filename,result_filename)
+
+
+cities  = ['MM','SG']
+times = ['2018102022']
+apptype =0   #all
+appsubtype =0  #all
+hql_filename = 'select_table.hql'
+result_filename = 'apptype_speeed.csv'
+#analyze_speed_apptype(cities,times,apptype,appsubtype,hql_filename,result_filename,percentile='y')
+
+cities  = ['MM']
+times = ['2018102322']
+hql_filename = 'select_table.hql'
+result_filename = 'dnsxdr.csv'
+#collect_domainname(cities,times,hql_filename,result_filename)
+
+cities  = ['MM']
+times = ['2018102322']
+hql_filename = 'select_table.hql'
+result_filename = 'dnsxdr.csv'
+#collect_domainname(cities,times,hql_filename,result_filename)
+ip = '106.75.26.39'
+#inquire_ip2domain(cities,times,ip)
+
+cities  = ['MM']
+times = ['2018102322']
+hql_filename = 'select_table.hql'
+result_filename = 'dnsxdr.csv'
+domain = 'www.baidu.com'
+#inquire_domain2ip(cities,times,domain)
+
+cities  = ['MM']
+times = ['2018102822']
+hql_filename = 'select_table.hql'
+result_filename = 'ip_speed_app_host.csv'
+ip_dec = ip_dot2dec('223.111.15.202')
+analyze_speed_ip_host(cities,times,ip_dec)
+
